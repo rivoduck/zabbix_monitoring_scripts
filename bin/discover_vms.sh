605b44a2e1597ca97138e78f139aec2dff236204
Andrea Rivetti <andrea.rivetti@top-ix.org>
2018-05-22 10:07:24 +0200

    fixed path

--- bin/discover_vms.sh
+++ bin/discover_vms.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+current_dir=$(dirname "$0")
 
+. ${current_dir}/functions.sh
+
+
+xen_detect
+if [ $? -eq 0 ]
+then
+    # error
+    exit -1
+else
+    if [ $? -eq 1 ]
+    then
+        # XenServer
+        ${current_dir}/discover_xenserver_vms.sh
+    else
+        # Xen
+        ${current_dir}/discover_xen_vms.sh
+    fi
+fi
