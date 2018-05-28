0d39f3a0cc479c364d1eefa8601082e439dad48b
Andrea Rivetti <andrea.rivetti@top-ix.org>
2018-05-28 16:12:50 +0200

    fixed path

--- bin/vms/discover.sh
+++ bin/vms/discover.sh
@@ -16,9 +16,9 @@ else
     if [ $res -eq 1 ]
     then
         # XenServer
-        python ${current_dir}/parse_vm_list_xenserver.py $vm_name
+        python ${current_dir}/xen/parse_vm_list_xenserver.py $vm_name
     else
         # Xen
-        php ${current_dir}/parse_vm_list_xen.php $vm_name
+        php ${current_dir}/xen/parse_vm_list_xen.php $vm_name
     fi
 fi
