90b47cc65819e582a77dde08278c006f6b33b317
Andrea Rivetti <andrea.rivetti@top-ix.org>
2018-05-29 11:01:06 +0200

    carry forward uuid when polling state for a VM that has disappeared

--- bin/vms/discover.sh
+++ bin/vms/discover.sh
@@ -18,9 +18,9 @@ else
     if [ $res -eq 1 ]
     then
         # XenServer
-        python ${current_dir}/xen/parse_vm_list_xenserver.py $vm_name $command
+        python ${current_dir}/xen/parse_vm_list_xenserver.py $vm_name $command $uuid
     else
         # Xen
-        php ${current_dir}/xen/parse_vm_list_xen.php $vm_name $command
+        php ${current_dir}/xen/parse_vm_list_xen.php $vm_name $command $uuid
     fi
 fi
