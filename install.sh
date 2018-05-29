37dfc7c7381aee989799921e3002be56250b5cd4
Andrea Rivetti <andrea.rivetti@top-ix.org>
2018-05-29 16:48:32 +0200

    bug

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ZABBIX_BASE_CONFDIR="/etc/zabbix/"
 ZABBIX_AGENT_CONF_D_1="zabbix_agentd.conf.d/"
 ZABBIX_AGENT_CONF_D_2="zabbix_agentd.d/"
 BUILD_BASE="/opt/zabbix_monitoring_scripts/"
+LOCAL_ZABBIX_AGENT_CONF_D="zabbix_agentd.conf.d/"
 
 NEWLINE=$'\n'
 
@@ -81,10 +82,10 @@ fi
 if [ "X${errors}" == "X" ]
 then
     # install files
-    install_file userparameter_topix_general.conf ${ZABBIX_BASE_CONFDIR}${ZABBIX_AGENT_CONF_D} ${BUILD_BASE}${ZABBIX_AGENT_CONF_D}
+    install_file userparameter_topix_general.conf ${ZABBIX_BASE_CONFDIR}${ZABBIX_AGENT_CONF_D} ${BUILD_BASE}${LOCAL_ZABBIX_AGENT_CONF_D}
 	if [ $vm_found -eq 1 ]
 	then
-        install_file userparameter_topix_vms.conf ${ZABBIX_BASE_CONFDIR}${ZABBIX_AGENT_CONF_D} ${BUILD_BASE}${ZABBIX_AGENT_CONF_D}
+        install_file userparameter_topix_vms.conf ${ZABBIX_BASE_CONFDIR}${ZABBIX_AGENT_CONF_D} ${BUILD_BASE}${LOCAL_ZABBIX_AGENT_CONF_D}
 	fi
 else
     # errors occourred
