a88b5aa77395829c4c8a60207664f8542dd1364e
Leonardo Sileo <leonardo.sileo@top-ix.org>
2025-05-29 08:55:24 +0200

    Aggiunto item per visualizzare valore di uscita del docker

---
+++ bin/docker/error_status.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import subprocess
+import json
+import sys
+import re
+
+def get_container_status(container_name):
+    result = subprocess.run(
+        ['docker', 'ps', '-a', '--filter', f'name={container_name}', '--format', '{{json .}}'],
+        capture_output=True, text=True
+    )
+    if not result.stdout.strip():
+        return "not found"
+
+    container_info = json.loads(result.stdout.strip())
+    status_str = container_info['Status'].lower()
+
+    if 'up' in status_str:
+        return 0
+    else:
+        match = re.search(r'\((\d+)\)', status_str)
+        code = int(match.group(1))
+        return code
+
+if __name__ == "__main__":
+    container_name = sys.argv[1]
+    print(get_container_status(container_name))
